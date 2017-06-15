7dce24ff057ae3cb9a52cf0a0df01592ff71ecf0
Database Administrator <codesql@users.noreply.github.com>
2017-06-15 10:14:44 -0600

    Create pga_sizing.sql

---
+++ oracle_scripts/pga_sizing.sql
@@ -0,0 +1,12 @@
+-- how is pga condition looks like
+select name, value from v$sysstat where name like  'workarea executions%';
+
+-- what do I have currently.
+-- while on it, look at cache hits as well.
+set lines 150;
+col name format a50;
+select * from v$pgastat;
+
+-- what using all my pga.
+select pid, program, pga_used_mem, pga_alloc_mem, pga_freeable_mem, pga_max_mem from v$process;
+
