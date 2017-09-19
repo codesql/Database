84294120c48846c423c1afe5faff1e42b7c508ef
Database Administrator <codesql@users.noreply.github.com>
2017-09-19 13:37:22 -0600

    Update slow_response.sql

--- oracle_scripts/slow_response.sql
+++ oracle_scripts/slow_response.sql
@@ -2,4 +2,8 @@
 select d.value as disk, m.value as memory, (d.value/m.value)*100 as ratio from v$sysstat m, v$sysstat d where m.name = 'sorts (memory)' and d.name ='sorts (disk)';
 
 --watch out for multipass
+--typically issue with respond time with large queries sorting
+--increase PGA_AGGREGATE_TARGET
+
+
 select name, value from v$sysstat where name like 'workarea executions%';
