3a940efa981d467a05502ce9c277aaf65adef39d
Database Administrator <codesql@users.noreply.github.com>
2017-09-26 15:25:42 -0600

    Upt2
    To check possible issue with multixactid or wraparound

--- postgres-scripts/check_wraparound_id.sql
+++ postgres-scripts/check_wraparound_id.sql
@@ -1,4 +1,5 @@
-The numbers should not be close to 4 billion or 2^32. If it is close, perform vacuum full.
+--The numbers should not be close to 4 billion or 2^32. The number should be fairly close to pg control data as well. If it is close, perform vacuum full.
+
 
 select txid_current(), txid_current_snapshot();
 
